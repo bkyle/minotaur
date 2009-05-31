71201113f5f367348004a2034b21bbb101cb0c6f
Bryan Kyle <bryan.kyle@gmail.com>
2009-05-31 08:04:16 -0700

    Cleaned up unused code, added -s/--source option

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,10 @@
+import java.io.ByteArrayInputStream;
 import java.io.File;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.MessageFormat;
 import java.util.Iterator;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Formatter;
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.LogRecord;
-import java.util.logging.Logger;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
@@ -21,35 +15,23 @@ import org.apache.commons.io.IOUtils;
 
 import parser.ASTNode;
 import parser.JavascriptParser;
-import parser.JavascriptParserVisitor;
 import parser.SimpleNode;
-
-import visitors.CompressingVisitor;
 
 public class Main {
 
-	private static final Logger LOGGER = Logger.getLogger("Main");
-	
-	static {
-		
-		Handler handler = new ConsoleHandler();
-		handler.setFormatter(new Formatter() {
-
-			@Override
-			public String format(LogRecord record) {
-				return record.getMessage();
-			}
-			
-		});
-		
-		LOGGER.addHandler(new ConsoleHandler());
-	}
-	
 	private static final Options OPTIONS = new Options();
 	
 	static {
+		/*
+		 *  Commands
+		 */
 		OPTIONS.addOption("c", "check", false, "Only check (validate) the source.");
 		OPTIONS.addOption("m", "minimize", false, "Minimize the source.");
+		
+		/*
+		 *  Options
+		 */
+		OPTIONS.addOption("s", "source", true, "Provide literal source instead of code from a file.");
 	}
 	
 	public static void main(String[] args) {
@@ -72,37 +54,51 @@ public class Main {
 		} else if (cl.hasOption('m')) {
 			command = new MinimizeCommand();
 		}
-			
-		if (cl.getArgList().size() > 0) {
-			Iterator i = cl.getArgList().iterator();
-			do {
-			
-				String name = (String) i.next();
-				File file = new File(name);
-				InputStream in = null;
-				try {
-					in = new FileInputStream(file);
-					JavascriptParser p = new JavascriptParser(in);
-					SimpleNode n = p.Program();
-					command.execute((ASTNode) n);
-				} catch (IOException e) {
-					System.err.println(MessageFormat.format("Could not find {0}", name));
-					System.exit(2);
-				} catch (parser.ParseException e) {
-					System.err.println(MessageFormat.format("Error parsing {0}\n{1}", name, e.getMessage()));
-					System.exit(1);
-				} finally {
-					IOUtils.closeQuietly(in);
-				}
-			} while (i.hasNext());
-			
-		} else {
-			JavascriptParser p = new JavascriptParser(System.in);
+	
+		if (cl.hasOption('s')) {
 			try {
+				String source = cl.getOptionValue('s');
+				ByteArrayInputStream in = new ByteArrayInputStream(source.getBytes());
+				JavascriptParser p = new JavascriptParser(in);
 				SimpleNode n = p.Program();
 				command.execute((ASTNode) n);
 			} catch (parser.ParseException e) {
-				LOGGER.log(Level.ALL, e.getMessage());
+				System.err.println(e.getMessage());
+				System.exit(1);
+			}
+		} else {
+			if (cl.getArgList().size() > 0) {
+				Iterator i = cl.getArgList().iterator();
+				do {
+				
+					String name = (String) i.next();
+					File file = new File(name);
+					InputStream in = null;
+					try {
+						in = new FileInputStream(file);
+						JavascriptParser p = new JavascriptParser(in);
+						SimpleNode n = p.Program();
+						command.execute((ASTNode) n);
+					} catch (IOException e) {
+						System.err.println(MessageFormat.format("Could not find {0}", name));
+						System.exit(2);
+					} catch (parser.ParseException e) {
+						System.err.println(MessageFormat.format("Error parsing {0}\n{1}", name, e.getMessage()));
+						System.exit(1);
+					} finally {
+						IOUtils.closeQuietly(in);
+					}
+				} while (i.hasNext());
+				
+			} else {
+				JavascriptParser p = new JavascriptParser(System.in);
+				try {
+					SimpleNode n = p.Program();
+					command.execute((ASTNode) n);
+				} catch (parser.ParseException e) {
+					System.err.println(e.getMessage());
+					System.exit(1);
+				}
 			}
 		}
 		
