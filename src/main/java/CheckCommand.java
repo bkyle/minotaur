34c536c821b1b332ae8f34b69b0db2bceb7ec281
Bryan Kyle <bryan.kyle@gmail.com>
2009-06-29 14:21:05 -0700

    Removed compressing operation from --check.

--- src/main/java/CheckCommand.java
+++ src/main/java/CheckCommand.java
@@ -2,11 +2,7 @@ import java.io.InputStream;
 
 import org.apache.commons.cli.CommandLine;
 
-import parser.ASTNode;
 import parser.JavascriptParser;
-import parser.Node;
-import parser.Visitor;
-import visitors.CompressingVisitor;
 
 
 public class CheckCommand implements Command {
@@ -21,9 +17,7 @@ public class CheckCommand implements Command {
 	public void execute(InputStream in) throws Throwable {
 		JavascriptParser parser = new JavascriptParser(in);
 		parser.setTracing(cl.hasOption("trace"));
-		Node node = parser.Program();
-		Visitor visitor = new CompressingVisitor(System.out);
-		node.accept(visitor, null);
+		parser.Program();
 	}
 
 }
